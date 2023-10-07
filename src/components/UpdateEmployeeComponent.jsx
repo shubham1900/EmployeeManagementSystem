ebfbb828afd50c9ce53c8541042c1d1a0b49e1c6
shubham1900 <shubham.tiwari19000@gmail.com>
2023-10-07 18:19:04 +0530

    debugging

--- src/components/UpdateEmployeeComponent.jsx
+++ src/components/UpdateEmployeeComponent.jsx
@@ -1,53 +1,55 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
- import { useParams } from 'react-router-dom';
- import { withParams } from './withParams';
-import EmployeeService from '../services/EmployeeService';
+  import { useParams } from 'react-router-dom';
+  import { withParams } from './withParams';
+ import EmployeeService from '../services/EmployeeService';
 
-export default class UpdateEmployeeComponent extends Component {
+ class UpdateEmployeeComponent extends Component {
     
     constructor(props){
-        
+      console.log(props)  
         super(props);
-        this.state={
-            //id:this.props.match.params.id,
-          firstName:"",
-          lastName:"",
-          email_Id:""
-        }
+         this.state={
+             id:this.props.match.params.id,
+           firstName:"",
+           lastName:"",
+           email_Id:""
+         }
         
 
-        this.changefirstNamehandler=this.changefirstNamehandler.bind(this);
-        this.changelastNamehandler=this.changelastNamehandler.bind(this);
-        //this.changeemail_Idhandler=this.changeemail_Idhandler.bind(this);
-        this.updateEmployee=this.updateEmployee.bind(this);
+         this.changefirstNamehandler=this.changefirstNamehandler.bind(this);
+         this.changelastNamehandler=this.changelastNamehandler.bind(this);
+         this.changeemail_Idhandler=this.changeemail_Idhandler.bind(this);
+         this.updateEmployee=this.updateEmployee.bind(this);
       
       }componentDidMount(){
-       // let {id}=useParams;
-        EmployeeService.getEmployeeById(this.props.match.params.id).then((res)=>{
-            let employee=res.data;
-            this.setState({firstName:employee.firstName,
-                lastName:employee.lastName,
-                email_Id:employee.email_Id
-            })
-        })
+       // let {id}=useParams
+       console.log(this.props)
+         EmployeeService.getEmployeeById(this.props.match.params.id).then((res)=>{
+             let employee=res.data;
+             this.setState({firstName:employee.firstName,
+                 lastName:employee.lastName,
+                 email_Id:employee.email_Id
+             })
+         })
     }
       updateEmployee=(e)=>{
-      e.preventDefault();
-      let employee={firstName:this.state.firstName,lastName:this.state.lastName,email_Id:this.state.email_Id}
-      console.log("employee=>" +JSON.stringify(employee))
+       e.preventDefault();
+      console.log(this.props)
+       let employee={firstName:this.state.firstName,lastName:this.state.lastName,email_Id:this.state.email_Id}
+       console.log("employee=>" +JSON.stringify(employee))
       
       
       }
-      changefirstNamehandler=(event)=>{
-      this.setState({firstName:event.target.value})
-      }
-      changelastNamehandler=(event)=>{
-        this.setState({lastName:event.target.value})
-        }
-        changeemail_Idhandler=(event)=>{
-          this.setState({email_Id:event.target.value})
-          }
+       changefirstNamehandler=(event)=>{
+       this.setState({firstName:event.target.value})
+       }
+       changelastNamehandler=(event)=>{
+         this.setState({lastName:event.target.value})
+         }
+         changeemail_Idhandler=(event)=>{
+           this.setState({email_Id:event.target.value})
+           }
        
           cancel() {
             // Use the Link component to navigate to the homepage
@@ -56,39 +58,42 @@ export default class UpdateEmployeeComponent extends Component {
           
         render() {
           return (
-            <div>
-            <div className="container"></div>  
-            <div className="row"></div>
-            <div className="card col-md-6 offset-md-3 offset-md-3">
-              <h3 className='text-center'>Update Employee</h3>
-              <div className='card-body'>
-                <form className='form-group'>
-                  <label>First Name:</label>
-                  <input placeholder='firstName' name='firstName' className='form-control'
-                  value={this.state.name} onChange={this.changefirstNamehandler}></input>
-                  <br></br>
+            
+             <div>
+             <div className="container"></div>  
+             <div className="row"></div>
+             <div className="card col-md-6 offset-md-3 offset-md-3">
+               <h3 className='text-center'>Update Employee</h3>
+               <div className='card-body'>
+                 <form className='form-group'>
+                   <label>First Name:</label>
+                   <input placeholder='firstName' name='firstName' className='form-control'
+                   value={this.state.name} onChange={this.changefirstNamehandler}></input>
+                   <br></br>
       
                   
-                  <label>Last Name</label>
-                   <input placeholder='lastName' name='lastName' className='form-control'
-                  value={this.state.name} onChange={this.changelastNamehandler}></input>
-                  <br></br>
+                   <label>Last Name</label>
+                    <input placeholder='lastName' name='lastName' className='form-control'
+                   value={this.state.name} onChange={this.changelastNamehandler}></input>
+                   <br></br>
       
                   
-                  <label>Email ID</label>
-                   <input placeholder='email_Id' name='email_Id' className='form-control'
-                  value={this.state.name} onChange={this.changeemail_Idhandler}></input>
-                  <br></br>
+                   <label>Email ID</label>
+                    <input placeholder='email_Id' name='email_Id' className='form-control'
+                   value={this.state.name} onChange={this.changeemail_Idhandler}></input>
+                   <br></br>
       
-                  <button className='btn btn-success' onClick={this.updateEmployee}>Save</button>
-                  <button className='btn btn-danger' style={{ marginLeft: "10px" }}>{this.cancel()}</button>
-                </form>
+                   <button className='btn btn-success' onClick={this.updateEmployee}>Save</button>
+                   <button className='btn btn-danger' style={{ marginLeft: "10px" }}>{this.cancel()}</button>
+                 </form>
                 
-              </div>
-            </div>
+               </div>
+             </div>
       
-            </div>
+             </div>
           )
         }
         
     }
+    export default withParams(UpdateEmployeeComponent);
+
--- src/components/withParams.js
+++ src/components/withParams.js
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 import { useParams } from "react-router-dom";
 
 export const withParams=(Component)=>(props)=>{
-    const params=useParams();
-    return <Component{...props}params={params}/>
+    // const params=useParams();
+    // return <Component{...props}params={params}/>
 }
